d910a5cadc35e2d211376123ee90f87a98c27ac7
yennobita <nguyenyen102003@gmail.com>
2023-03-12 10:17:25 +0700

    fix: code pug

--- src/common/header/Head.jsx
+++ src/common/header/Head.jsx
@@ -1,17 +1,17 @@
-import React from "react"
+import React from "react";
 
 const Head = () => {
   return (
     <>
-      <section className='head'>
-        <div className='container d_flex'>
-          <div className='left row'>
-            <i className='fa fa-phone'></i>
-            <label> +88012 3456 7894</label>
-            <i className='fa fa-envelope'></i>
-            <label> support@ui-lib.com</label>
+      <section className="head">
+        <div className="container d_flex">
+          <div className="left row">
+            <i className="fa fa-phone"></i>
+            <label> +84 123 456 789</label>
+            <i className="fa fa-envelope"></i>
+            <label>support@gmail.com</label>
           </div>
-          <div className='right row RText'>
+          <div className="right row RText">
             <label>Theme FAQ"s</label>
             <label>Need Help?</label>
             <span>🏳️‍⚧️</span>
@@ -22,7 +22,7 @@ const Head = () => {
         </div>
       </section>
     </>
-  )
-}
+  );
+};
 
-export default Head
+export default Head;
